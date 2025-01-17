c77870acaa0523734998ef3af550852eb4f2d047
Ngo Ngoc Nam <namngocngo22@gmail.com>
2025-01-17 15:12:28 +0700

    edit domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ['placehold.co','img.vietqr.io','localhost'], // Allow 'placehold.co'
+        domains: ['placehold.co','img.vietqr.io','localhost','api-cafe.226pay.com','cafe.226pay.com'], // Allow 'placehold.co'
         dangerouslyAllowSVG: true, // Enable SVG support
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", // Add additional security
     },
